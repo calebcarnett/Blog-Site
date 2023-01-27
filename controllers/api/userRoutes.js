696c0db32507669d90901a617b75f4b4407ccb48
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-01-27 11:44:54 -0500

    updated routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -11,31 +11,11 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
+
+router.post('/', async (req, res) => {
   try {
-    const userData = await User.findByPk(req.params.id, {
-    });
+    const userData = await User.create(req.body);
 
-    if (!userData) {
-      res.status(404).json({ message: 'No location found with this id!' });
-      return;
-    }
-
-    res.status(200).json(userData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-
-
-router.post('/signup', async (req, res) => {
-  try {
-    const userData = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
@@ -47,9 +27,14 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+
+
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where:
+       { 
+        email: req.body.email,
+       } });
 
     if (!userData) {
       res
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -79,13 +79,22 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/dashboard', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-    return;
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
-  res.render('dashboard');
 });
 
 router.get('/comment', (req, res) => {
