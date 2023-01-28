d8056268e9756738ab4e181e03b7d26cacb51f0b
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-01-28 07:42:12 -0500

    added boostrap styling, fixed dashboard rendering issue, working on post route issue

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -55,12 +55,31 @@ router.get('/post/:id', async (req, res) => {
 });
 
 
+
 router.get('/dashboard', async (req, res) => {
-    res.render('/dashboard');
-    return;
- 
+  try {
+    // Get all posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('dashboard', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-  
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
